f41944bb9e7598513591ea4e2ee8bb6317a30061
Andrea Falzi <af@andreafalzi.dev>
2022-08-21 20:59:15 +0200

    db fix + creating repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import dataHeroes from './heroes.json';
+import abilitiesHeroes from './abilities.json';
 
 function App() {
+  const [heroes, setHeroes] = useState([]);
+  const [abilities, setAbilities] = useState([]);
+  const url = 'https://cdn.cloudflare.steamstatic.com/';
+
+  useEffect(() => {
+    setHeroes(dataHeroes);
+    setAbilities(abilitiesHeroes);
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Heroes</h1>
+      <div className='grid'>
+        {heroes.map(({ localized_name, name, img, id, icon }) => {
+          return (
+            <div key={id}>
+              <h2>
+                <img src={`${url}${icon}`} alt={name} />
+                {localized_name}
+              </h2>
+              <img src={`${url}${img}`} alt={name} />
+            </div>
+          );
+        })}
+      </div>
+      <h1>Abilities</h1>
+      <div className='grid'>
+        {abilities.map(({ dname, desc, img }, index) => {
+          return (
+            <div key={index}>
+              <h2>{dname}</h2>
+              <img src={`${url}${img}`} alt={dname} />
+              <p>{desc}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
