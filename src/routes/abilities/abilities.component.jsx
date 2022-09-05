6d91c3ace1546ddd1957c1aceaf69349e6d75c04
Andrea Falzi <andrea.falzi1988@gmail.com>
2022-09-05 13:51:38 +0200

    abilities css

--- src/routes/abilities/abilities.component.jsx
+++ src/routes/abilities/abilities.component.jsx
@@ -1,19 +1,58 @@
 import './abilities.style.scss';
 
 export const Abilities = ({ abilities, url, className }) => {
-  console.log(abilities.length);
+  const newAbilities = [];
+  const checkAbilities = (abilities) => {
+    abilities.forEach((ability) => {
+      if (ability.mc === undefined) {
+        ability = { ...ability, mc: ['0'] };
+      }
+      if (!Array.isArray(ability.mc)) {
+        ability = { ...ability, mc: [ability.mc] };
+      }
+      if (ability.cd === undefined) {
+        ability = { ...ability, cd: ['0'] };
+      }
+      if (!Array.isArray(ability.cd)) {
+        ability = { ...ability, cd: [ability.cd] };
+      }
+      newAbilities.push(ability);
+    });
+  };
+
+  checkAbilities(abilities);
+
   return (
     <>
       <h1>Abilities</h1>
       <div className={className}>
-        {abilities.map(({ dname, desc, img, mc, cd }, index) => {
+        {newAbilities.map(({ dname, desc, img, mc, cd }, index) => {
           return (
             <div key={index} className='ability-card'>
               <h2>{dname}</h2>
-              <img src={`${url}${img}`} alt={dname} />
-              <p>{desc}</p>
-              {mc !== undefined && <p>Mana Cost: {mc}</p>}
-              {cd !== undefined && cd !== '0' && <p>Cooldown:{cd}</p>}
+              <div className='content'>
+                <img src={`${url}${img}`} alt={dname} />
+                <div className='inside-content'>
+                  <p>{desc}</p>
+                  {!mc.includes('0') && (
+                    <p className='strong'>
+                      Mana:
+                      {mc.map((e) => (
+                        <span> {e}</span>
+                      ))}
+                    </p>
+                  )}
+
+                  {!cd.includes('0') && (
+                    <p className='strong'>
+                      Cooldown:
+                      {cd.map((e) => (
+                        <span> {e}</span>
+                      ))}
+                    </p>
+                  )}
+                </div>
+              </div>
             </div>
           );
         })}
